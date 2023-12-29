d4b4755908316c349ce8302ee50479f5929f7c1c
Fernando González Gaete <83770169+FerkoxGG@users.noreply.github.com>
2023-12-29 18:58:47 -0300

    Changed the locale of the Angular application to Spanish (Chile) and added pipes to format dates in different locales.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
 
+// Configuración del locale de la app
+import localeESCL from "@angular/common/locales/es-CL";
+import localeFRCA from "@angular/common/locales/fr-CA";
+
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localeESCL);
+registerLocaleData(localeFRCA);
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SharedModule,
-        BrowserAnimationsModule,
-    ]
+  declarations: [AppComponent],
+  providers: [
+    {
+      // Cambiar el local de la aplicación
+      provide: LOCALE_ID,
+      useValue: "es-CL"
+    }
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule
+  ]
 })
-export class AppModule { }
+export class AppModule {}
